39ac0d587449d85bd9245e482c8f5ab7cf41f603
Ubuntu <ubuntu@ip-10-0-2-151.ec2.internal>
2018-08-29 03:59:51 +0000

    Sample Job

---
+++ create-job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+
+#kubectl apply -f roles-job.yml
+
+TOKEN=$(kubectl describe secret $(kubectl get secrets | grep default | cut -f1 -d ' ') | grep -E '^token' | cut -f2 -d':' | tr -d '\t' | xargs )
+
+curl -v -k \
+	--header "Authorization: Bearer $TOKEN" \
+	-X DELETE https://api.useast1.cis.kuberbots.com/apis/batch/v1/namespaces/jobs-test/jobs/job-wq-2
+
+curl -v -k \
+	--header "Content-Type: application/json" \
+	--header "Authorization: Bearer $TOKEN" \
+	 https://api.useast1.cis.kuberbots.com/apis/batch/v1/namespaces/jobs-test/jobs \
+	-d @job.json
+
